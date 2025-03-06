e0eb85c26999c4a982c2c2a401849fd44231a50a
EasySouls <93796810+EasySouls@users.noreply.github.com>
2025-03-06 18:54:42 +0000

    tanfolyam part 2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ticketing API 2025')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   const port = process.env.PORT ?? 3000;
   console.log(`Nestjs is running on port ${port}`);
